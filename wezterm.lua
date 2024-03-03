a8e2f41dcd376b6d1b6c9fe86b8dd4625f1292bc
Felix Vadcard <vadcard.felix@gmail.com>
2024-03-03 19:32:15 +0100

    removing background image i dont know if i will use it again

--- wezterm.lua
+++ wezterm.lua
@@ -11,18 +11,18 @@ local light_theme = {
 
 local dark_theme = {
   color_scheme = 'astromouse (terminal.sexy)',
-  window_background_image_hsb = {
-    -- Darken the background image by reducing it to 1/3rd
-    brightness = 0.3,
-
-    -- You can adjust the hue by scaling its value.
-    -- a multiplier of 1.0 leaves the value unchanged.
-    hue = 1.0,
-
-    -- You can adjust the saturation also.
-    saturation = 1.0,
-  },
-  window_background_image = os.getenv('HOME') .. '/.config/wezterm/wezterm_assets/lampadere.jpg',
+  -- window_background_image_hsb = {
+  --   -- Darken the background image by reducing it to 1/3rd
+  --   brightness = 0.3,
+  --
+  --   -- You can adjust the hue by scaling its value.
+  --   -- a multiplier of 1.0 leaves the value unchanged.
+  --   hue = 1.0,
+  --
+  --   -- You can adjust the saturation also.
+  --   saturation = 1.0,
+  -- },
+  -- window_background_image = os.getenv('HOME') .. '/.config/wezterm/wezterm_assets/lampadere.jpg',
 }
 
 local fonts = {
